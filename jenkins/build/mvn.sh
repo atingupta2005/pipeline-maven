733dba1569d0ef9699082bb42835df7519cd09a8
Atin Gupta <atingupta2005@gmail.com>
2021-05-16 12:45:55 +0530

    -

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,7 +4,7 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-WORKSPACE=/var/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=/var/jenkins_home/workspace/pipeline-maven
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
 #docker run --rm  -v  $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
